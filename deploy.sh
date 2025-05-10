7f9fabec7d6dec1a6a6a316fb50ce9050570257c
peitalin <n6378056@gmail.com>
2025-05-10 13:11:05 +0900

    gitignore

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cargo near deploy build-reproducible-wasm yellow-loong.testnet \
+	without-init-call \
+	network-config testnet \
+	sign-with-plaintext-private-key \
+	--signer-public-key $NEAR_SIGNER_PUBLIC_KEY \
+	--signer-private-key $NEAR_SIGNER_PRIVATE_KEY \
+	send
