31f3ac6d89d2959d2f7a865d83136e0b43474ffc
peitalin <n6378056@gmail.com>
2025-05-17 10:09:54 +0900

    test: Initialize contract in test_basics_on

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source .env
 
-cargo near deploy build-reproducible-wasm $NEAR_SIGNER_ACCOUNT_ID \
+cargo near deploy build-reproducible-wasm $CONTRACT_ACCOUNT_ID \
 	without-init-call \
 	network-config testnet \
 	sign-with-plaintext-private-key \
