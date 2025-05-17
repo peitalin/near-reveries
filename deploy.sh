6589207d1bfd6f4792e83a28d76ade9ef8210618
peitalin <n6378056@gmail.com>
2025-05-17 10:54:52 +0900

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ source .env
 
 # Deploy the contract
 cargo near deploy build-reproducible-wasm $CONTRACT_ACCOUNT_ID \
-	with-init-call new '{"trusted_account": "$NEAR_SIGNER_ACCOUNT_ID"}' \
+	with-init-call new json-args '{"trusted_account": "$CONTRACT_ACCOUNT_ID"}' \
+	prepaid-gas '100.0 Tgas' \
+	attached-deposit '0 NEAR' \
 	network-config testnet \
 	sign-with-plaintext-private-key \
 	--signer-public-key $NEAR_SIGNER_PUBLIC_KEY \
