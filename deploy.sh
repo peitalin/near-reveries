69baa6a760ad867ae80f1a9befd36a2fa6f54073
peitalin <n6378056@gmail.com>
2025-05-17 10:12:46 +0900

    deploy: Add contract initialization step after deployment in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source .env
 
+# Deploy the contract
 cargo near deploy build-reproducible-wasm $CONTRACT_ACCOUNT_ID \
 	without-init-call \
 	network-config testnet \
@@ -8,3 +9,6 @@ cargo near deploy build-reproducible-wasm $CONTRACT_ACCOUNT_ID \
 	--signer-public-key $NEAR_SIGNER_PUBLIC_KEY \
 	--signer-private-key $NEAR_SIGNER_PRIVATE_KEY \
 	send
+
+# Initialize the contract
+near call $CONTRACT_ACCOUNT_ID new '{"trusted_account": "$NEAR_SIGNER_ACCOUNT_ID"}' --accountId $NEAR_SIGNER_ACCOUNT_ID --networkId testnet
