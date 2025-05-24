9080ca9677d0c2bafc7f72179d862c870ebb0fbf
peitalin <n6378056@gmail.com>
2025-05-24 19:15:56 +0900

    script

--- deploy-passkey.sh
+++ deploy-passkey.sh
@@ -4,7 +4,7 @@ source .env
 cd ./passkey_controller
 # Deploy the contract
 cargo near deploy build-reproducible-wasm $PASSKEY_CONTROLLER_CONTRACT_ID \
-	with-init-call new json-args "{\"trusted_account\": \"$NEAR_SIGNER_ACCOUNT_ID\"}" \
+	with-init-call new json-args "{\\"trusted_relayer_account_id\\": \\"$NEAR_RELAYER_ACCOUNT_ID\\", \\"owner_id\\": \\"$NEAR_SIGNER_ACCOUNT_ID\\", \\"initial_passkey_pks\\": null}" \
 	prepaid-gas '100.0 Tgas' \
 	attached-deposit '0 NEAR' \
 	network-config testnet \
